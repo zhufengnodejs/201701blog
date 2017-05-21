f36642eb00b7d18a153b32e1cfd04c7c05305384
zhufengnodejs <zhufengnodejs@126.com>
2017-05-21 17:45:04 +0800

    15. 实现搜索功能

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,19 @@ let {Article} = require('../model');
 let router = express.Router();
 //当客户端通过GET请求的方式访问 /路径的时候，会交由对应的函数来处理
 router.get('/',function(req,res){
+    let {keyword} = req.query;
+    let query = {};
+    if(keyword){
+        //query.title = new RegExp(keyword);// /b/
+        query['$or'] = [{title:new RegExp(keyword)},
+            {content:new RegExp(keyword)}];
+    }
     //populate可以把一个字段从字符串转成对象
-    Article.find().populate('user').exec(function(err,articles){
+    Article.find(query).populate('user').exec(function(err,articles){
         //路由是相对路径，相对于模板根目录
-        res.render('index',{title:'首页',articles});
+        res.render('index',{title:'首页',
+            keyword,
+            articles});
     });
 });
 module.exports = router;
