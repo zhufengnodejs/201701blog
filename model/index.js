6761396cf07aa547c75440d97a6156a45681b662
zhufengnodejs <zhufengnodejs@126.com>
2017-05-21 13:04:43 +0800

    10. 新增发表文章

--- model/index.js
+++ model/index.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose');
+let ObjectId = mongoose.Schema.Types.ObjectId;
 //连接数据库
 mongoose.connect('mongodb://127.0.0.1/201701blog');
 // 定义用户集合的骨架模型，规定了用户集合中文档的属性和类型
@@ -12,3 +13,12 @@ let UserSchema = new mongoose.Schema({
 let User = mongoose.model('User',UserSchema);
 //把用户模型挂载到导出对象上
 exports.User = User;
+let ArticleSchema = new mongoose.Schema({
+    title:String,//标题
+    content:{type:String},//正文 内容
+    createAt:{type:Date,default:Date.now},//创建时间
+    //user是一个外键，引用的是另外一个集合(User)的主键
+    user:{type:ObjectId,ref:'User'}//作者，引用的是用户表的主键(_id)
+});
+let Article = mongoose.model('Article',ArticleSchema);
+exports.Article = Article;
--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,24 @@
 let express = require('express');
 let {checkLogin,checkNotLogin} = require('../auth');
 let router = express.Router();
+let {Article} = require('../model');
 //  /article/add
 router.get('/add',checkLogin,function(req,res){
     res.render('article/add',{title:'发表文章'});
+});
+router.post('/add',checkLogin,function(req,res){
+    let article = req.body;
+    //这篇文章的作者是当前的登录用户
+    article.user = req.session.user._id;
+    Article.create(article,function(err,doc){
+        if(err){
+            req.flash('error',err);
+            res.redirect('back');
+        }else{
+            req.flash('success','文章发表成功');
+            res.redirect('/');
+        }
+    })
 });
 // module model
 module.exports = router;
